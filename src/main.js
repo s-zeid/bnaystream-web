da88ac0faf7238a9712f4870953bd71486e8b93a
Scott Zeid <s@zeid.me>
2015-11-18 09:32:22 -0600

    [src/main] only require server after dependencies are installed

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 var childProcess = require("child_process");
 
-var server = require("./server/server.js");
 var common = require("./common.js");
 
 
@@ -13,6 +12,8 @@ childProcess.spawn("make", [], {
  
  var host = "127.0.0.1";
  var port = process.argv[2] || 26299;
+ 
+ var server = require("./server/server.js");
  process.stderr.write("Listening on <http://" + host + ":" + port + "/>...\n");
  server.app.listen(port);
 });
